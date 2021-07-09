a86868e2d8694d8697e2aa6eab3dda927e0602f2
Siddhartha Pothukuchi <spothu3469@gmail.com>
2021-07-09 19:25:53 -0400

    Entered the first sloka for Sri Krishna Karnamrutham.

--- assets/data/books.js
+++ assets/data/books.js
@@ -1,12 +1,12 @@
 export default [
   {
     id: "b001",
-    name: "ఒకటవ పుస్తకము",
+    name: "శ్రీకృష్ణ కర్ణామృతం",
     image: require("../images/book1.png"),
-  },
+  }/*,
   {
     id: "b002",
     name: "రెండొవ పుస్తకము",
     image: require("../images/book2.png"),
-  },
+  },*/
 ];
