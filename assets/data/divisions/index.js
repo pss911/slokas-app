3c8e1c09d448c5473fdebf2361fad550a77b3242
Siddhartha Pothukuchi <spothu3469@gmail.com>
2021-07-09 19:35:21 -0400

    Fixed the issue with index.js to have only the active section.

--- assets/data/divisions/index.js
+++ assets/data/divisions/index.js
@@ -1,4 +1,4 @@
 export default {
-  b001: require("../divisions/b001").default,
-  b002: require("../divisions/b002").default,
+  b001: require("../divisions/b001").default/*,
+  b002: require("../divisions/b002").default,*/
 };
