a4672ae9214337aca8a8e4d956693494e8842716
shanmukha-pothukuchi <shanmukha.pothukuchi@gmail.com>
2021-09-26 13:54:17 -0400

    Files for the first production release.

--- assets/data/books.js
+++ assets/data/books.js
@@ -17,28 +17,4 @@
     image: require("../images/Sivanandalahari.jpg"),
     hasDivisions: false,
   },
-  {
-    id: "b004",
-    name: "మూక పంచశతి",
-    image: require("../images/Mukapanchasathi.jpg"),
-    hasDivisions: true,
-  },
-  {
-    id: "b005",
-    name: "ఆర్యా ద్విశతి",
-    image: require("../images/Aryadvisathi.jpg"),
-    hasDivisions: false,
-  },
-  {
-    id: "b006",
-    name: "నారాయణీయము",
-    image: require("../images/Narayaneeyamu.jpg"),
-    hasDivisions: true,
-  },
-  {
-    id: "b007",
-    name: "ముకుందమాల",
-    image: require("../images/Mukundamala.jpg"),
-    hasDivisions: false,
-  },
 ];
--- assets/data/divisions/index.js
+++ assets/data/divisions/index.js
@@ -1,5 +1,3 @@
 export default {
   b001: require("../divisions/b001").default,
-  b004: require("../divisions/b004").default,
-  b006: require("../divisions/b006").default,
 };
